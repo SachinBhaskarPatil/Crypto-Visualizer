9c3177c353630fc45fe0a3c7ce687c4ace910093
Sachin Bhaskar Patil <128968099+SachinBhaskarPatil@users.noreply.github.com>
2023-12-21 22:39:46 +0530

    Add files via upload

---
+++ src/config/firebaseConfig.jsx
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyASZgGf1j4HPtYDBKrDgNPThSGpp0MBr8w",
+  authDomain: "crypto-visualizer-c5ad7.firebaseapp.com",
+  projectId: "crypto-visualizer-c5ad7",
+  storageBucket: "crypto-visualizer-c5ad7.appspot.com",
+  messagingSenderId: "569735712982",
+  appId: "1:569735712982:web:7aab4c7adabb0f352fb977",
+  measurementId: "G-8K329KR6CS"
+};
+
+export default firebaseConfig;
+
+
